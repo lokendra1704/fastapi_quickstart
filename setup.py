4cec7348243b6027eaaa776b398f4f7c1f109a8f
Lokendra Bohra <2017kucp1038@iiitkota.ac.in>
2021-02-27 14:29:22 +0530

    +

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='fastapi-quickstart',
+    version='1.0.0',
+    py_modules=['fastapi_quickstart'],
+    author='Lokendra Bairwa',
+    author_email='lokendrabairwa@gmail.com',
+    url='www.lokendrabohra.tech',
+    description='Common Helper functions which gets you kickstart with your fastapi development.'
+)
